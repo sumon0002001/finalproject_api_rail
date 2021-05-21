03363e19ba8f7ebf5c42f64cce7c357556e6315c
sumon0002001 <59405831+sumon0002001@users.noreply.github.com>
2021-05-21 11:32:13 +0600

    Added all data

--- db/schema.rb
+++ db/schema.rb
@@ -2,36 +2,47 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# This file is the source Rails uses to define your schema when running `bin/rails
-# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_12_200343) do
+ActiveRecord::Schema.define(version: 2020_06_18_204122) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "airlines", force: :cascade do |t|
-    t.string "name"
-    t.string "img_url"
-    t.string "slug"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.string "title"
+  create_table "illnesses", force: :cascade do |t|
+    t.bigint "user_id", null: false
     t.string "description"
-    t.integer "score"
-    t.bigint "airline_id", null: false
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["airline_id"], name: "index_reviews_on_airline_id"
+    t.index ["user_id"], name: "index_illnesses_on_user_id"
   end
 
-  add_foreign_key "reviews", "airlines"
+  create_table "trackings", force: :cascade do |t|
+    t.bigint "illness_id", null: false
+    t.datetime "date"
+    t.string "mood"
+    t.float "temperature"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "medicines", default: [], array: true
+    t.string "symptons", default: [], array: true
+    t.index ["illness_id"], name: "index_trackings_on_illness_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  add_foreign_key "illnesses", "users"
+  add_foreign_key "trackings", "illnesses"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,48 +1,9 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
 
-airlines = Airline.create([
-    { 
-      name: "Novo Air",
-      img_url: "https://images.app.goo.gl/NdEdjYu6ukzswqdo6"
-    }, 
-    { 
-      name: "Regent Airways",
-      img_url: "https://images.app.goo.gl/zvvjrLxgo3xj5T2g9"
-    },
-    { 
-      name: "Us-Bangla Airlines",
-      img_url: "https://images.app.goo.gl/FDjsxuQcKNs78uq29" 
-    }, 
-    { 
-      name: "Biman Bangla Airlines",
-      img_url: "https://images.app.goo.gl/oDkv3nkyYG7HB5Dh9" 
-    }, 
-    { 
-      name: "Jet Airways",
-      img_url: "https://images.app.goo.gl/qtsxSdRMjzUqdEZs8" 
-    }, 
-    { 
-      name: "Spice Jet",
-      img_url: "https://images.app.goo.gl/xNPsa4ny9bPSBH8T8" 
-    }
-  ])
+user1 = User.create(username: 'sumon', password:'123456')
+user2 = User.create(username: 'sumon', password: '123456')
 
-reviews = Review.create([
-  {
-    title: 'Its nice experience',
-    description: 'I had a lovely time',
-    score: 5,
-    airline: airlines.first
-  },
-  {
-    title: 'Its worst experience',
-    description: 'I had a bad time',
-    score: airlines.first
-  }  
-])
+illness1 = Illness.create(user_id: 1, name: 'Colitis', description: 'Stomache')
+illness2 = Illness.create(user_id: 1, name: 'Headache')
+
+tracking1 = Tracking.create(illness_id:1 ,date: "2020-05-11" , mood:'🙂', temperature: 36.5, medicines:["Omeprazol", "Peptobismol"], symptons:["Weakness", "Stomache"])
+tracking2 = Tracking.create(illness_id: 1, date: "2020-05-10" , mood:'🙁', temperature: 36.7)
